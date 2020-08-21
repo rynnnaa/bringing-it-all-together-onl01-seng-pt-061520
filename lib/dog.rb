83de2e96176bfba6aee8f37cd57b8a3d6e1b69d7
Ryna Truong <rynatruong@outlook.com>
2020-08-21 23:42:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -37,10 +37,8 @@ class Dog
   end
   
   def self.new_from_db(row)
-    id = row[0]
-    name = row[1]
-    breed = row[2]
-    self.new(id: id, name: name, breed: breed)
+    attributes = {id: row[0], name: row[1], breed: row[2]}
+        self.new(attributes)
   end
   
   def self.find_by_id(id)
