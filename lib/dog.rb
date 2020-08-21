301d4c036706f432762085fcc407be6b29a2269f
Ryna Truong <rynatruong@outlook.com>
2020-08-21 23:38:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -60,7 +60,8 @@ class Dog
       dog_data = dog[0]
       dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
     else
-      self.create(attributes)
+      dog = self.create(attributes)
     end
+    dog
   end
 end
