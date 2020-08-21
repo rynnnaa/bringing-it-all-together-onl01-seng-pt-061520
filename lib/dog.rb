51fd0f2b8849eb22db8e69a50c459b8b265951d1
Ryna Truong <rynatruong@outlook.com>
2020-08-21 23:38:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -58,7 +58,7 @@ class Dog
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", attributes[:name], attributes[:breed])
     if !dog.empty?
       dog_data = dog[0]
-      dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
+      self.new_from_db(dog_data)
     else
       dog = self.create(attributes)
     end
