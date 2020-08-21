46c7472f539a678ad140c9973a764d86b66f3657
Ryna Truong <rynatruong@outlook.com>
2020-08-21 23:33:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -55,6 +55,14 @@ class Dog
   end
   
   def find_or_create_by_id(name:, breed:)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?" name, breed)
+    if !dog.empty?
+      dog_data = dog[0]
+      dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
+    else
+      dog = self.create(name: name, breed: breed)
+    end
+  end
     
     
 end
